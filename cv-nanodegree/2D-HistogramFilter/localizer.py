3198715292d0411cf6ab250d34775936bf44ba33
Anurag Sharma <anurag89sharma@gmail.com>
2019-06-07 16:29:40 +0530

    complted notebook exercises and fixed bug in localizer

--- cv-nanodegree/2D-HistogramFilter/localizer.py
+++ cv-nanodegree/2D-HistogramFilter/localizer.py
@@ -16,21 +16,31 @@ def initialize_beliefs(grid):
 
 def sense(color, grid, beliefs, p_hit, p_miss):
     new_beliefs = []
+    for i in range(len(grid)):
+        tmp_list = list()
+        for j in range(len(grid[i])):
+            hit = (color == grid[i][j])
+            tmp_list.append(beliefs[i][j] * (hit * p_hit + (1-hit) * p_miss))
+            
+        new_beliefs.append(tmp_list)
 
-    #
-    # TODO - implement this in part 2
-    #
+    total_sum = sum([sum(x) for x in new_beliefs])
+    
+    for i in range(len(new_beliefs)):
+        for j in range(len(new_beliefs[i])):
+            new_beliefs[i][j] = new_beliefs[i][j] / total_sum
 
     return new_beliefs
 
 def move(dy, dx, beliefs, blurring):
     height = len(beliefs)
     width = len(beliefs[0])
-    new_G = [[0.0 for i in range(width)] for j in range(height)]
+    new_G = [[0.0 for i in range(height)] for j in range(width)]
     for i, row in enumerate(beliefs):
         for j, cell in enumerate(row):
             new_i = (i + dy ) % width
             new_j = (j + dx ) % height
-            # pdb.set_trace()
             new_G[int(new_i)][int(new_j)] = cell
+            #import pdb; pdb.set_trace()
+            
     return blur(new_G, blurring)
